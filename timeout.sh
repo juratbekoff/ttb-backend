9cffee618a1cb63d9eb503797e89778680f20fd0
Ibrohim Turdiyev <Ibrohimdev03@gmail.com>
2023-06-20 10:11:53 +0500

    Create timeout.sh

---
+++ timeout.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Execute the command passed as an argument and terminate it after a certain duration
+
+# Set the timeout duration (in seconds)
+TIMEOUT_DURATION=300
+
+# Start the command in the background
+"$@" &
+
+# Get the PID (process ID) of the command
+CMD_PID=$!
+
+# Wait for the specified duration
+sleep "$TIMEOUT_DURATION"
+
+# Check if the command is still running
+if kill -0 "$CMD_PID" >/dev/null 2>&1; then
+  # The command is still running, terminate it
+  kill -9 "$CMD_PID"
+fi
